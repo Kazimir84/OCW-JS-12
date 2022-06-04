593aad1ccf5feeaf89f89c6a8cd044377fa3fb02
Kazimir84 <kav2007@ukr.net>
2022-06-04 16:43:34 +0300

    Fixed

--- js/main.js
+++ js/main.js
@@ -1,57 +1,100 @@
-// 1.
+//======================================================================================================================
+//                                                 Class Work
+//                                               OKTEN-CW-12-JS
+//======================================================================================================================
+//                                                      1
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
 // зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+let colorRed = Math.floor((Math.random()*255));
+let colorGreen = Math.floor((Math.random()*255));
+let colorBlue = Math.floor((Math.random()*255));
+let color = `rgb(${colorRed}, ${colorGreen}, ${colorBlue})`;
+let borderColor = `rgb(${colorGreen}, ${colorRed}, ${colorBlue})`;
+let buttonColor = `rgb(${colorBlue}, ${colorGreen}, ${colorRed})`;
+
+let wrapper = document.createElement('div');
+    document.body.append(wrapper);
+
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(jsonResponse => {
-        let div = document.createElement('DIV');
-        document.body.append(div);
-        let ol = document.createElement('OL');
+        let div = document.createElement('div');
+        wrapper.append(div);
+        let ol = document.createElement('ol');
         div.append(ol);
 
-        let selected = document.createElement('OL');
-        selected.style = `
-                        position: fixed;
-                        left: 50%;                        
-                        top: 10%;                        
-        `
-        document.body.append(selected);
+        let selected = document.createElement('ol');
+        selected.style = ` 
+                        position: fixed;                      
+                        left: 50%;                       
+        `;
+        document.body.prepend(selected);
 
         for (let jsonResponseElement of jsonResponse) {
-            let li = document.createElement('LI');
-            let btn = document.createElement('BUTTON');
+            let li = document.createElement('li');
+                li.style = `
+                            border: 1px solid ${color};
+                            background-color: ${borderColor};
+                            padding: 10px;
+                            margin: 10px;
+                `;
+            let btn = document.createElement('button');
+                btn.style = `
+                            background-color: ${buttonColor};
+                            margin-top: 10px;
+                            padding: 10px;
+                            border: 1px double ${color};
+                            border-radius: 15px;
+                `;
             btn.innerText = 'Click Me';
 
             ol.append(li)
-            li.innerText = `User: ${jsonResponseElement.userId} 
-                            ID:${jsonResponseElement.id} 
-                            Title: ${jsonResponseElement.title} 
-                            Body:${jsonResponseElement.body}`
+            li.innerText = `User: ${jsonResponseElement.userId};
+                            ID: ${jsonResponseElement.id};
+                            Title: ${jsonResponseElement.title};
+                            Body: ${jsonResponseElement.body};
+            `;
             li.appendChild(btn);
-
             let id = jsonResponseElement.userId;
+            let commentt = document.createElement('ul');
+                commentt.style = `
+                                listStyle = 'none';
+                                display: flex;
+                `;
+            let divWrap = document.createElement('div');
+                divWrap.style =`
+                                display: flex;
+                `;
+                divWrap.className = 'hidden';
+                li.append(divWrap);
 
-            btn.addEventListener('click', function () {
-                fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-                    .then(response => response.json())
-                    .then(comments => {
-                        let li = document.createElement('LI');
-                        selected.append(li);
-                        li.style = `                       
-                                   border: 2px solid #ff9800;
-                                   `
-                        for (let comment of comments) {
-                            console.log('dasds', id)
-                            console.log('dasdsadas', comment.postId)
-                            if (id === comment.postId) {
-                                li.innerText = `ID: ${comment.id}
-                                                Name: ${comment.name}
-                                                Email: ${comment.email}
-                                                Body: ${comment.body}`;
+            btn.addEventListener('click', function (e) {
+                divWrap.classList.toggle('hidden')
+                if (!selected.contains(commentt)) {
+                    fetch(`https://jsonplaceholder.typicode.com/comments`)
+                        .then(response => response.json())
+                        .then(comments => {
+                            for (let comment of comments) {
+                                let div = document.createElement('div');
+                                    div.style = `                       
+                                                border: 2px solid ${borderColor}; 
+                                                background-color: ${color};
+                                                padding: 10px;
+                                                margin: 5px;
+                                                width: 20%;
+                                    `;
+                                if (id === comment.postId) {
+                                    div.innerText = `ID: ${comment.id};
+                                                     Name: ${comment.name};
+                                                     Email: ${comment.email};
+                                                     Body: ${comment.body}`;
+                                    divWrap.append(div);
+                                };
                             };
-                        };
-                    });
+                        });
+                } else {commentt.remove()};
+                selected.append(commentt);
             });
         };
     });
